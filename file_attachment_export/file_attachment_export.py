33fd19f22f007d9c957810a1de79bf816964c6fa
Josh Fullmer <joshfullmer@me.com>
2018-02-27 09:57:29 -0700

    Add file attachment import

--- file_attachment_export/file_attachment_export.py
+++ file_attachment_export/file_attachment_export.py
@@ -6,6 +6,13 @@ and puts all of those files into a ZIP file.
 The ZIP file will also have a "files.csv" which will outline
 all of the other details about the files, including who they were
 attached to, by contact ID
+
+INSTRUCTIONS
+============
+
+1. Set the appname and API key below
+2. Run in terminal
+3. Zip file is placed in the 'output/' folder.
 """
 import base64
 import csv
---
+++ file_attachment_import/file_attachment_import.py
@@ -0,0 +1,82 @@
+"""
+This application will import files from a folder of attachments and
+a CSV that outlines the filename and who the file should be attached
+to.
+
+INSTRUCTIONS
+============
+
+1. Set the appname and API key below
+2. Add the attachments to the attachments folder, in a
+   folder called "APPNAME_attachments/"
+3. Add the attachments CSV to the attachments folder,
+   named "APPNAME_attachments.csv", with the following required fields
+   a. Contact ID as 'id'
+   b. Desired filename as 'filename'
+   c. Filename as is exists in the folder as 'filepath'
+      I. This may be the same as 'b.'
+   d. File extension as 'extension'
+4. Run in terminal.
+"""
+import base64
+import csv
+import os
+
+from infusionsoft.library import Infusionsoft
+
+from constants import QJ154_APIKEY, SUPPORTED_FILE_TYPES
+
+# SET APPNAME AND API KEY
+appname = 'qj154'
+api_key = QJ154_APIKEY
+
+infusionsoft = Infusionsoft(appname, api_key)
+
+dir_path = "attachments/{}_attachments".format(appname)
+
+if not os.path.isdir(dir_path):
+    raise NotADirectoryError("Please put the files in "
+                             "'attachments/{}_attachments'".format(appname))
+
+csv_path = "attachments/{}_attachments.csv".format(appname)
+
+if not os.path.isfile(csv_path):
+    raise FileNotFoundError("Please name the CSV '{}_attachments.csv' "
+                            "and put it in 'attachments/'".format(appname))
+
+with open(csv_path, newline='', encoding='utf-8-sig') as csvfile:
+    reader = csv.DictReader(csvfile)
+
+    if sorted(reader.fieldnames) != sorted(['id', 'filename',
+                                            'filepath', 'extension']):
+        raise ValueError("Incorrect column header names.  Must be:"
+                         "'id', 'filename', 'filepath', and 'extension'")
+
+    file_num = 0
+    for row in reader:
+        file_num += 1
+        file_path = "/".join([dir_path, row['filepath']])
+
+        print("")
+        print("File #{}".format(file_num))
+        print("{} - {}".format(file_path,
+                               os.path.isfile(file_path)))
+        print("Filesize (in MB): {}".format(os.path.getsize(file_path)/2**20))
+        print("{}: {}".format(row['id'], row['filename']))
+        print("Supported File Type? {}".format(row['extension'].lower()
+                                               in SUPPORTED_FILE_TYPES))
+
+        if ((not os.path.isfile(file_path)) or
+                (os.path.getsize(file_path)/2**20 > 9.5) or
+                (row.get('extension') is None) or
+                (row['extension'] == '') or
+                (row['extension'].lower() not in SUPPORTED_FILE_TYPES)):
+            continue
+
+        file = open(file_path, 'rb')
+        resp = infusionsoft.FileService('uploadFile',
+                                        int(row['id']),
+                                        row['filename'],
+                                        base64.b64encode(file.read())
+                                              .decode('ascii'))
+        print("File created! FileBoxId: {}".format(resp))
