6a593df3684456b9dd3dc7f18f023e3ab16fba69
Josh Fullmer <joshfullmer@me.com>
2018-02-27 15:24:21 -0700

    Add v1 of Free Trial Transfer

---
+++ free_app_data_transfer/free_app_data_transfer.py
@@ -0,0 +1,83 @@
+from infusionsoft.library import Infusionsoft
+
+from constants import (QJ154_APIKEY, TE361_APIKEY, FIELDS, DATATYPES)
+from infusionsoft_actions import get_table, create_custom_field
+
+SOURCE_APPNAME = 'qj154'
+SOURCE_API_KEY = QJ154_APIKEY
+DESTINATION_APPNAME = 'te361'
+DESTINATION_API_KEY = TE361_APIKEY
+
+CONTACTS_WITH_TAG_IDS = []
+
+src_infusionsoft = Infusionsoft(SOURCE_APPNAME, SOURCE_API_KEY)
+dest_infusionsoft = Infusionsoft(DESTINATION_APPNAME, DESTINATION_API_KEY)
+
+"""
+Contacts
+Contact Custom Fields
+Tags
+Tag Assign
+Contact Action
+"""
+
+src_contact_id = create_custom_field(dest_infusionsoft,
+                                     'Source App Contact ID')['Name']
+
+contact_fields = FIELDS['Contact'][:]
+custom_fields = get_table(src_infusionsoft, 'DataFormField')
+rename_mapping = {}
+for custom_field in custom_fields:
+    if custom_field['FormId'] == -1 and custom_field['DataType'] != 23:
+        contact_fields += ["_{}".format(custom_field['Name'])]
+
+        field = create_custom_field(dest_infusionsoft,
+                                    custom_field['Label'],
+                                    'Contact',
+                                    DATATYPES[custom_field['DataType']],
+                                    custom_field.get('Values'))
+
+        rename_mapping["_" + custom_field['Name']] = field['Name']
+
+query = {}
+if CONTACTS_WITH_TAG_IDS:
+    contact_ids = []
+    for tag in get_table(src_infusionsoft,
+                         'ContactGroupAssign',
+                         {},
+                         ['ContactId', 'GroupId']):
+        if tag['GroupId'] in CONTACTS_WITH_TAG_IDS:
+            contact_ids += [tag['ContactId']]
+    query['Id'] = list(set(contact_ids))
+
+existing_contacts = {}
+for contact_id in get_table(dest_infusionsoft,
+                            'Contact',
+                            {src_contact_id: '_%'},
+                            ['Id', src_contact_id]):
+    existing_contacts[int(contact_id[src_contact_id])] = contact_id['Id']
+
+contact_fields.remove("_Listboxtest")
+
+contacts = get_table(src_infusionsoft, 'Contact', query, contact_fields)
+for contact in contacts:
+    if existing_contacts.get(int(contact['Id'])):
+        print("Contact already exists: Contact ID {}".format(
+            existing_contacts[contact['Id']]))
+        continue
+    if contact['Id'] == contact['CompanyID']:
+        print("The contact is a company (ID: {})... skipping".format(
+            contact['CompanyID']))
+        continue
+    for key in contact_fields:
+        if rename_mapping.get(key):
+            contact[rename_mapping[key]] = contact.get(key)
+            if contact.get(key):
+                del contact[key]
+    contact[src_contact_id] = str(contact['Id'])
+    contact = {k: v for k, v in contact.items() if (v and v != 'null')}
+    created_contact_id = dest_infusionsoft.ContactService('add', contact)
+    print("Contact created!  Contact ID:"
+          " {} - {} {}".format(created_contact_id,
+                               contact['FirstName'],
+                               contact['LastName']))
---
+++ free_app_data_transfer/infusionsoft_actions.py
@@ -0,0 +1,75 @@
+from constants import FIELDS, CF_FORM_ID
+
+
+def get_table(infusionsoft, tablename, query={}, fields=[]):
+    lookup_fields = []
+    if fields == []:
+        lookup_fields += FIELDS[tablename][:]
+    else:
+        lookup_fields = fields
+    table = []
+    page = 0
+    while True:
+        table_page = infusionsoft.DataService('query',
+                                              tablename,
+                                              1000,
+                                              page,
+                                              query,
+                                              lookup_fields)
+        table += table_page
+        if len(table_page) < 1000:
+            break
+        page += 1
+    print("{} table returned {} records".format(tablename, len(table)))
+    return table
+
+
+def create_custom_field(infusionsoft, fieldname, tablename='Contact',
+                        fieldtype='Text', values=None):
+    query = {'Label': fieldname, 'FormId': CF_FORM_ID[tablename]}
+    existing_fields = infusionsoft.DataService('query',
+                                               'DataFormField',
+                                               1000,
+                                               0,
+                                               query,
+                                               FIELDS['DataFormField'])
+    existing_field = []
+    for f in existing_fields:
+        if f['FormId'] == CF_FORM_ID[tablename]:
+            existing_field += [f]
+
+    field = {}
+    if not existing_field:
+        tab_id = infusionsoft.DataService('query',
+                                          'DataFormTab',
+                                          1000,
+                                          0,
+                                          {'FormId': CF_FORM_ID[tablename]},
+                                          FIELDS['DataFormTab'])[0]['Id']
+        header_id = infusionsoft.DataService('query',
+                                             'DataFormGroup',
+                                             1000,
+                                             0,
+                                             {'TabId': tab_id},
+                                             FIELDS['DataFormGroup'])[0]['Id']
+        field['Id'] = infusionsoft.DataService('addCustomField',
+                                               tablename,
+                                               fieldname,
+                                               fieldtype,
+                                               header_id)
+        field['Name'] = "_" + infusionsoft.DataService('query',
+                                                       'DataFormField',
+                                                       1000,
+                                                       0,
+                                                       query,
+                                                       ['Name'])[0]['Name']
+        if values:
+            infusionsoft.DataService('updateCustomField',
+                                     field['Id'],
+                                     values)
+        print("Created field {}".format(field['Name']))
+    else:
+        field['Id'] = existing_field[0]['Id']
+        field['Name'] = "_" + existing_field[0]['Name']
+        print("Field {} already exists.\n".format(field['Name']))
+    return field
