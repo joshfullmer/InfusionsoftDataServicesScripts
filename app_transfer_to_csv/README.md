194c85e91958026144f16c21aebd91064512a019
Josh Fullmer <joshfullmer@me.com>
2018-03-05 10:53:54 -0700

    Add readme

---
+++ app_transfer_to_csv/README.md
@@ -0,0 +1,23 @@
+INSTRUCTIONS
+============
+
+1. Fill in the source and destination appnames and API keys found in config.py
+2. Indicate which of the items need to be transferred by setting the individual settings in config.py to True or False accordingly (must be capitalized)
+3. Check the Additional settings in config.py for things that may aid you in importing the CSVs
+4. If you are transferring any of the below, create a custom field tab in the destination app, so the API can create custom fields
+- Contacts
+- Companies
+- Contact Actions
+- Opportunities
+- Orders
+5. Run step1 in the terminal
+- Change directory into 'app_transfer_to_csv/'
+- Run 'python step1.py'
+6. Grab the ZIP file that's created in app_transfer_to_csv/output' for step1 and import all things for step1
+7. Run step2 in the terminal (same as #5)
+8. Import step2 (same as #6)
+9. Run step3 in the terminal (same as #5)
+10. Import step3 (same as #6)
+
+NOTE: You can tell which things are transferred by step in the config.py file.  
+If the customer doesn't need anything from step 3 or step 2, you can skip running those steps.
--- app_transfer_to_csv/config.py
+++ app_transfer_to_csv/config.py
@@ -1,3 +1,9 @@
+"""
+This contains all of the choices and settings for the
+entire app_transfer_to_csv application
+
+"""
+
 from constants import QJ154_APIKEY, TE361_APIKEY
 
 SOURCE_APPNAME = 'qj154'
@@ -5,31 +11,46 @@ SOURCE_API_KEY = QJ154_APIKEY
 DESTINATION_APPNAME = 'te361'
 DESTINATION_API_KEY = TE361_APIKEY
 
+# Step 1
 CONTACTS = True
-CONTACTS_WITH_TAG_IDS = []
 COMPANIES = True
-# Set to False if you are going to create custom fields yourself.  This is
-# used when the customer doesn't want all custom fields moved.
-#
-# This applies to company custom fields as well
-CREATE_CUSTOM_FIELDS = True
 TAGS = True
-# Set to True if you want a Tag CSV to import instead of automatically creating
-# tags through API
-TAGS_CSV = False
 PRODUCTS = True
-# Feed the app a list of Product IDs to skip transferring, in case the customer
-# doesn't want all of the products transferred.
-# Comma-separated list of IDs.
-PRODUCTS_TO_SKIP = []
+
+# Step 2
 NOTES = True
 TASKS_APPOINTMENTS = True
 OPPORTUNITIES = True
 ORDERS = True
 SUBSCRIPTIONS = True
+
+# Step 3
+# ORDER ITEMS
+# ORDER PAYMENTS
+
+# ADDITIONAL SETTINGS
+# ===================
+
+# This setting allows you to provide a list of tag IDs. The app will only 
+# add contacts with that tag ID to the CSV for importing
+CONTACTS_WITH_TAG_IDS = []
+
+# Set to False if you are going to create custom fields yourself.  This is
+# used when the customer doesn't want all custom fields moved.
+# NOTE: This applies to company custom fields as well
+CREATE_CUSTOM_FIELDS = True
+
+# Set to True if you want a Tag CSV to import instead of automatically creating
+# tags through API
+TAGS_CSV = False
+
+# Feed the app a list of Product IDs to skip transferring, in case the customer
+# doesn't want all of the products transferred.
+# Comma-separated list of IDs.
+PRODUCTS_TO_SKIP = []
+
 # This will shift the next bill dates of subscriptions if the customer
 # needs time in their old app to cancel subscriptions.  No subscriptions will
 # bill before the cutoff date you specify below
-
 # Format: "YYYY/MM/DD"
 SUBSCRIPTION_CUT_OFF_DATE = ''
