9928dfe3227fdf19e7ca60fbbf56ecd6ecce4d22
Josh Fullmer <joshfullmer@me.com>
2018-03-06 10:23:21 -0700

    Add Email History Export Readme

---
+++ email_history_export/README.md
@@ -0,0 +1,54 @@
+# Instructions
+
+This program will export all of the files from the given Infusionsoft
+application that have been modified using the below SQL and indicated to
+be email history files.
+
+The output will be a ZIP file that contains each individual email history
+file as a .html file
+
+Make sure to add the comma separated list of email file attachment IDs
+in 'file_ids.py' that is given from the SQL
+
+## SQL
+
+### Export Email History
+
+#### Select the email data
+```sql
+SELECT EmailSent.Id,EmailSent.ContactId,EmailSent.EmailAddress,
+EmailSent.DateCreated AS 'DateSent',Mail.Subject,Mail.FromAddress,Mail.FromName,MailContent.FileBoxId AS 'EmailMessageId'
+FROM EmailSent
+INNER JOIN MailContent
+ON EmailSent.EmailId=MailContent.EmailId
+INNER JOIN Mail
+ON EmailSent.EmailId=Mail.Id WHERE MailContent.FileBoxId > 0;
+```
+
+#### Always backup in case you jack it up
+```sql
+CREATE TABLE FileBox_20180214 AS (SELECT * FROM FileBox);
+```
+#### Creates an ID to use
+```sql
+CREATE TABLE FileBoxEmailId_20180214 AS (SELECT Id FROM FileBox WHERE FileName LIKE 'email%');
+```
+
+#### Changes a few columns to work with export
+```sql
+UPDATE FileBox
+SET FileName=CONCAT(FileName,'.html'),
+Category = 'docs',
+Extension = 'html'
+WHERE Id IN (SELECT Id FROM FileBoxEmailId_20180214);
+```
+
+#### This changes everything back
+```sql
+UPDATE FileBox
+SET FileName=LEFT(FileName,LENGTH(FileName)-5),
+Category = 'email',
+Extension = '',
+FileBoxType = 3
+WHERE Id IN (SELECT Id FROM FileBoxEmailId_20180214);
+```
--- email_history_export/email_history_export.py
+++ email_history_export/email_history_export.py
@@ -1,46 +1,6 @@
 """
-This program will export all of the files from the given Infusionsoft
-application that have been modified using the below SQL and indicated to
-be email history files.
-
-The output will be a ZIP file that contains each individual email history
-file as a .html file
-
-Make sure to add the comma separated list of email file attachment IDs
-in 'file_ids.py' that is given from the SQL
-
-SQL
-====================
-# Export Email History
-
-# Select the email data
-SELECT EmailSent.Id,EmailSent.ContactId,EmailSent.EmailAddress,
-EmailSent.DateCreated AS 'DateSent',Mail.Subject,Mail.FromAddress,Mail.FromName,MailContent.FileBoxId AS 'EmailMessageId'
-FROM EmailSent
-INNER JOIN MailContent
-ON EmailSent.EmailId=MailContent.EmailId
-INNER JOIN Mail
-ON EmailSent.EmailId=Mail.Id WHERE MailContent.FileBoxId > 0;
-
-# Always backup in case you jack it up
-CREATE TABLE FileBox_20180214 AS (SELECT * FROM FileBox);
-# Creates an ID to use
-CREATE TABLE FileBoxEmailId_20180214 AS (SELECT Id FROM FileBox WHERE FileName LIKE 'email%');
-
-# Changes a few columns to work with export
-UPDATE FileBox
-SET FileName=CONCAT(FileName,'.html'),
-Category = 'docs',
-Extension = 'html'
-WHERE Id IN (SELECT Id FROM FileBoxEmailId_20180214);
-
-# This changes everything back
-UPDATE FileBox
-SET FileName=LEFT(FileName,LENGTH(FileName)-5),
-Category = 'email',
-Extension = '',
-FileBoxType = 3
-WHERE Id IN (SELECT Id FROM FileBoxEmailId_20180214);
+This program exports all email history items and puts them into a zipfile
+that's found in the '/output' folder.
 
 """
 
