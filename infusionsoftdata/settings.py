e69e8fbcd40ba285e861129c97f6129d33475f64
Josh Fullmer <joshfullmer9@gmail.com>
2018-09-07 13:59:15 -0700

    Heroku

--- infusionsoftdata/settings.py
+++ infusionsoftdata/settings.py
@@ -129,11 +129,4 @@ USE_TZ = True
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
 
-# Extra places for collectstatic to find static files.
-STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT, 'static'),
-)
-
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
-
 django_heroku.settings(locals())
