52ab475ec031084b9c685bb1e3b47287985ccc88
Josh Fullmer <joshfullmer9@gmail.com>
2018-09-18 12:22:11 -0700

    Slack testing (csrf)

--- infusionsoftdata/views.py
+++ infusionsoftdata/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
+from django.views.decorators.csrf import csrf_exempt
 
 
 def home(request):
     return render(request, 'home.html')
 
 
+@csrf_exempt
 def slack(request):
     print(request)
