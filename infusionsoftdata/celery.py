349879f46c0ebd7993597f5250e61ae46be84ddf
Josh Fullmer <joshfullmer9@gmail.com>
2018-09-19 11:21:25 -0700

    Celery testing v4

--- infusionsoftdata/celery.py
+++ infusionsoftdata/celery.py
@@ -5,6 +5,9 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'infusionsoftdata.settings')
 
 app = Celery('infusionsoftdata')
 
+app.conf.update(BROKER_URL=os.environ['REDIS_URL'],
+                CELERY_RESULT_BACKEND=os.environ['REDIS_URL'])
+
 app.config_from_object('django.conf:settings', namespace='CELERY')
 
 app.autodiscover_tasks()
